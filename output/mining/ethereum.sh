471c01556652169a8a07a9867f9046229a0ca25d
Mark Philip Philipsen <markpp@gmail.com>
2021-03-03 10:25:52 +0100

    fixed -> dynamic user name and id

--- output/mining/ethereum.sh
+++ output/mining/ethereum.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ethminer -P stratum1+tcp://0x64944b4eA741DAd66bE664a57Ef2E56c87b1F5f7.MP3090@eu1.ethermine.org:4444 --cuda-block-size 128 --cuda-grid-size 16384
+ethminer -P stratum1+tcp://0x64944b4eA741DAd66bE664a57Ef2E56c87b1F5f7.AAU3090@eu1.ethermine.org:4444
--- output/mining/user.sh
+++ output/mining/user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USER_ID=${LOCAL_USER_ID:-9001}
-echo 'Starting with username : markpp and UID : $USER_ID'
-useradd -s /bin/bash -u $USER_ID -o -c '' -m markpp
-export HOME=/home/markpp
-su markpp bash -c '/usr/local/bin/ethereum.sh';'bash'
+echo 'Starting with username : aau and UID : -9001 host -> 1000'
+useradd -s /bin/bash -u $USER_ID -o -c '' -m aau
+export HOME=/home/aau
+su aau bash -c '/usr/local/bin/ethereum.sh';'bash'
